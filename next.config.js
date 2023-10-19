71b31b38585e4df45859d0a18cee36671ad8f6fb
surajgoraya <SurajGoraya@outlook.com>
2023-10-19 16:01:45 -0400

    build: :construction_worker: make comp. with app router, adjust app conf

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    //Needed for static builds on gh-pages and the eventual public server.
+    output: 'export',
+    trailingSlash: true,
+}
 
 module.exports = nextConfig
