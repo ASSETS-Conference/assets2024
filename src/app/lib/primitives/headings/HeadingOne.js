76fb9c47e89537426286e1adb05c094ae38790e2
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2023-10-22 20:52:17 -0400

    refactor: :recycle: refactored to allow colour to be passed as a prop

--- src/app/lib/primitives/headings/HeadingOne.js
+++ src/app/lib/primitives/headings/HeadingOne.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
-export default function HeadingOne({children}) {
-  return (
-    <h1 className='font-bold text-2xl md:text-3xl xl:text-5xl'>{children}</h1>
-  )
+export default function HeadingOne({
+    children,
+    colour = "text-primary-text-dark",
+}) {
+    return (
+        <h1 className={`font-bold text-2xl md:text-3xl xl:text-5xl ${colour}`}>
+            {children}
+        </h1>
+    );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,27 @@
-import Container  from './lib/primitives/Container'
-import Footer from './lib/primitives/Footer';
+import Hero from "./lib/components/Hero";
+import Container from "./lib/primitives/Container";
+import Footer from "./lib/primitives/Footer";
 import Section from "./lib/primitives/Section";
-import HeadingOne from './lib/primitives/headings/HeadingOne';
-import HeadingThree from './lib/primitives/headings/HeadingThree';
-import HeadingTwo from './lib/primitives/headings/HeadingTwo';
+import HeadingOne from "./lib/primitives/headings/HeadingOne";
+import HeadingThree from "./lib/primitives/headings/HeadingThree";
+import HeadingTwo from "./lib/primitives/headings/HeadingTwo";
 
 export default function Home() {
-  return (
-    <>
-    <main id="content">
-      <Container>
-        <Section title="ASSETS at a Glance">
-          <p>hello primitives</p>
-        </Section>
-        <Section title="Important Dates">
-          <HeadingOne>h1</HeadingOne>
-          <HeadingTwo>h2</HeadingTwo>
-          <HeadingThree>h3</HeadingThree>
-          <p>hello primitives</p>
-        </Section>
-      </Container>
-    </main>
-    </>
-  );
+    return (
+        <>
+            <main id="content">
+                <Container>
+                    <Section title="ASSETS at a Glance">
+                        <p>hello primitives</p>
+                    </Section>
+                    <Section title="Important Dates">
+                        <HeadingOne>h1</HeadingOne>
+                        <HeadingTwo>h2</HeadingTwo>
+                        <HeadingThree>h3</HeadingThree>
+                        <p>hello primitives</p>
+                    </Section>
+                </Container>
+            </main>
+        </>
+    );
 }
