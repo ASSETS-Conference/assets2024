db70ba3712be9066ab9c221afdc384d7bfd97450
surajgoraya <SurajGoraya@outlook.com>
2023-10-23 19:09:51 -0400

    fix: :lipstick: make hero resize better (thx @jpreet927)

--- src/app/lib/components/Hero.js
+++ src/app/lib/components/Hero.js
@@ -4,14 +4,14 @@ import HeroCard from "./HeroCard";
 
 export default function Hero() {
     return (
-        <section className="bg-hero-img w-full h-screen bg-cover bg-center relative">
-            <Container>
-                <div className="z-[20] h-screen w-full flex items-center justify-center lg:justify-start">
+        <section className="bg-hero-img w-full min-h-[65vh] py-20 bg-cover bg-[center_center_1rem] relative flex items-center justify-start">
+            <Container className={'flex-1'}>
+                <div className="z-[20] w-full h-[100%] flex items-center justify-center lg:justify-start aspect aspect-video max-w-6xl">
                     <HeroCard />
                 </div>
             </Container>
-            <div className="absolute top-0 left-0 w-full h-[50%] bg-gradient-to-b from-black/80 to-black/0" />
-            <div className="absolute bottom-0 left-0 w-full h-[50%] bg-gradient-to-t from-black/30 to-black/0" />
+            <div aria-hidden="true" className="absolute top-0 left-0 w-full h-[50%] bg-gradient-to-b from-black/80 to-black/0" />
+            <div aria-hidden="true" className="absolute bottom-0 left-0 w-full h-[50%] bg-gradient-to-t from-black/30 to-black/0" />
         </section>
     );
 }
