0d6227762dda0560c36582c1bb3046eff1972646
surajgoraya <SurajGoraya@outlook.com>
2023-10-23 14:59:00 -0400

    feat: :sparkles: menu detects pages

--- src/app/lib/components/primitives/Nav.js
+++ src/app/lib/components/primitives/Nav.js
@@ -1,12 +1,19 @@
+'use client'
 import React from 'react'
+import { usePathname } from 'next/navigation'
 
 export default function Nav() {
+  const _MENU_DATA = [
+    {title: 'Home', href: '/'},
+    {title: 'Code of Conduct', href: '/code-of-conduct/'},
+    {title: 'Organizing Committee', href: '/committees/organizing-committee/'},
+
+  ]
+
   return (
     <nav className="min-w-full p-8 fixed top-0 z-[998] backdrop-blur-sm text-theme-off-white backdrop-blur-md">
         <ul className="min-h-full min-w-full flex flex-row justify-center items-center gap-16 font-medium">
-            <li ><a href='/'>Home</a></li>
-            <li><a href='/code-of-conduct'>Code of Conduct</a></li>
-            <li><a href='/committees/organizing-committee'>Organizing Committee</a></li>
+          {_MENU_DATA.map((data, i)=><li><a key={`menu_item-${i}`} className={`underline hoctive:decoration-4 ${usePathname() === data.href ? 'decoration-4' : ''}`} href={data.href}>{data.title}</a></li>)}
         </ul>
     </nav>
   )
