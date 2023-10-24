5450dc52d29a95ca741eba0fdaefbc69e89d8d1d
surajgoraya <SurajGoraya@outlook.com>
2023-10-23 23:21:15 -0400

    feat: :sparkles: added date component (fully functional)

--- src/app/lib/components/DateList.js
+++ src/app/lib/components/DateList.js
@@ -1,29 +1,54 @@
 import React from "react";
 
-export default function DateList() {
-  return (
-    's'
+export function DateList({ dates }) {
+  return dates && dates.length ? (
+    <>
+      {dates.map((date, i, all) => {
+        return (
+          <MonthContainer
+            key={`month_container-${i}`}
+            isLast={all.length-1 === i}
+            monthName={date.month}
+            year={date.year}
+            eventItems={date.eventItems}
+          />
+        );
+      })}
+    </>
+  ) : (
+    <div className="w-[100%] flex flex-col md:flex-row items-start">
+      {/* <div className="flex-1">
+        <p className="h3">{' '}</p>
+      </div> */}
+      <div className="flex-1 opacity-40">
+        <p className="font-light h3 my-2">
+          {"Dates aren't out just yet! "}&mdash; check back later!
+        </p>
+      </div>
+    </div>
   );
 }
-export function MonthContainer({monthName, year, eventItems }) {
-    return (
-        <div className="pt-10 w-[100%] flex flex-col md:flex-row items-start">
-        <div className="flex-1">
-          <p className="h2 uppercase">{`${monthName} ${year}`}</p>
-        </div>
-        <div className="flex-1 border-b-[1px] md:border-b-[2px] border-black w-[100%]">
-          
-        </div>
+export function MonthContainer({ monthName, year, eventItems, isLast }) {
+  return (
+    <div className="pt-10 w-[100%] flex flex-col md:flex-row items-start">
+      <div className="flex-1">
+        <p className="h2 uppercase">{`${monthName} ${year}`}</p>
       </div>
-    )
+      <div className={`flex-1 border-black w-[100%] ${isLast ? `border-b-[0px]` : 'border-b-[1px] md:border-b-[2px]'}`}>
+        {eventItems.map((event, i) => {
+          return <EventItem key={`event-item-${i}`} title={event.title} date={event.date} time={event.time} />;
+        })}
+      </div>
+    </div>
+  );
 }
-export function EventItem({date}) {
+export function EventItem({ title, date, time }) {
   return (
     <div className="mb-8">
-      <p className="font-medium my-2 text-xl md:mt-2 md:text-2xl">title</p>
+      <p className="font-medium my-2 text-xl md:mt-2 md:text-2xl">{title}</p>
       <div className="flex flex-row gap-8">
-        <p>03.10.2023</p>
-        <p>11:59PM AOE</p>
+        <p>{date}</p>
+        <p>{time} AOE</p>
       </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,41 @@
-import DateList from "./lib/components/DateList";
+import { DateList } from "./lib/components/DateList";
 import Hero from "./lib/components/Hero";
 import Container from "./lib/components/primitives/Container";
 import Link from "./lib/components/primitives/Link";
 import Section from "./lib/components/primitives/Section";
+import { CONFERENCE_DATES } from "./lib/config/homepage.config";
 
 export default function Home() {
   return (
     <>
-    <main id="">
-      <Hero/>
-      <Container id="content">
-      <Section title="ASSETS at a Glance" spacing={'standard'}>
-        <div className="flex flex-col gap-y-5">
-            <p>{`The 25th International ACM SIGACCESS Conference on Computers and Accessibility.`}</p>
-            <p>{`The ASSETS conference is the premier forum for presenting research on the design, evaluation, use, and education related to computing for people with disabilities and older adults. For those in Europe and Oceania, ASSETS is rated as Core A —a designation for the top academic conferences that are "highly respected in a discipline area" (Core A; Top 16%).`}</p>
-            <p>{`We invite high-quality original submissions on topics relevant to computing and accessibility. All contributions are peer-reviewed by an international Program Committee. Accepted papers and the abstracts for posters and demonstrations, experience reports, and the student research competition will be archived in the ACM Digital Library. Authors of selected papers will be invited to submit extended versions of their papers to a special issue of the`} <Link href='https://dl.acm.org/journal/taccess' target = '_blank' colour={'primary'}>{`ACM Transactions on Accessible Computing (TACCESS)`}</Link>.</p>
-            <p>{`*The in-person full conference events will take place from October 23 to October 25. However, invitation-only events (i.e. the Doctoral Consortium) will take place on October 22.`}</p>
-        </div>
-      </Section>
-        <Section title="Important Dates" spacing={'bottom-only'}>
-          <p><strong>All deadlines</strong> are 11:59 P.M. Anywhere on Earth (UTC -12:00).</p>
-          <DateList/>
-        </Section>
-      </Container>
-    </main>
+      <main id="">
+        <Hero />
+        <Container id="content">
+        <Section title="Important Dates" spacing={"standard"}>
+            <p>
+              <strong>All deadlines</strong> are 11:59 P.M. Anywhere on Earth
+              (UTC -12:00).
+            </p>
+            <DateList dates={CONFERENCE_DATES}/>
+          </Section>
+          <Section title="ASSETS at a Glance" spacing={"bottom-only"}>
+            <div className="flex flex-col gap-y-4 max-w-[75ch]">
+              <p>{`The 25th International ACM SIGACCESS Conference on Computers and Accessibility.`}</p>
+              <p>{`The ASSETS conference is the premier forum for presenting research on the design, evaluation, use, and education related to computing for people with disabilities and older adults. For those in Europe and Oceania, ASSETS is rated as Core A —a designation for the top academic conferences that are "highly respected in a discipline area" (Core A; Top 16%).`}</p>
+              <p>
+                {`We invite high-quality original submissions on topics relevant to computing and accessibility. All contributions are peer-reviewed by an international Program Committee. Accepted papers and the abstracts for posters and demonstrations, experience reports, and the student research competition will be archived in the ACM Digital Library. Authors of selected papers will be invited to submit extended versions of their papers to a special issue of the`}{" "}
+                <Link
+                  href="https://dl.acm.org/journal/taccess"
+                  target="_blank"
+                  colour={"primary"}
+                >{`ACM Transactions on Accessible Computing (TACCESS)`}</Link>
+                .
+              </p>
+            </div>
+          </Section>
+     
+        </Container>
+      </main>
     </>
   );
 }
