b35df1cb345d624cb7a1f9997bfc2b8334d07146
surajgoraya <SurajGoraya@outlook.com>
2024-01-03 20:38:34 -0500

    fix: :lipstick: reduce spacing btwn sections
    thought it was a little too much. + fixed issue where arrows would compress when there were multi-lines

--- src/app/lib/components/primitives/Section.js
+++ src/app/lib/components/primitives/Section.js
@@ -12,16 +12,16 @@ export default function Section({ title, children, className, spacing, id }) {
 
     switch (spacing) {
       case 'standard':
-        setSpacing = 'py-16 md:py-20'
+        setSpacing = 'py-16 md:py-18'
         break;
       case 'top-only':
-        setSpacing = 'pt-16 md:pt-20'
+        setSpacing = 'pt-16 md:pt-18'
         break;
       case 'bottom-only':
-        setSpacing = 'pb-16 md:pb-20'
+        setSpacing = 'pb-16 md:pb-18'
         break;
       default:
-        setSpacing = 'py-16 md:py-20'
+        setSpacing = 'py-16 md:py-18'
         break;
     }
     
@@ -31,7 +31,7 @@ export default function Section({ title, children, className, spacing, id }) {
   return (
     <section className={`${evalSpacing()} ${className}`}>
       <div className='mt-7 border-b-[2px] border-black'>
-        <h1  id={id ? id : generateID()}  className='flex flex-row justify-flex-start content-center gap-3 mb-6 max-w-[30ch]'><FaArrowRight aria-hidden className='mt-[.1rem]'/>{title}</h1>
+        <h1  id={id ? id : generateID()}  className='flex flex-row justify-flex-start content-center gap-3 mb-6 max-w-[30ch]'><FaArrowRight aria-hidden className='mt-[.1rem] min-w-[1em] aspect-square'/>{title}</h1>
       </div>
       <div className='mt-7'>
         {children}
