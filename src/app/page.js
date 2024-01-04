1163117b37873314c75819d7cd230153ddc03422
surajgoraya <SurajGoraya@outlook.com>
2024-01-03 20:36:38 -0500

    feat: :lipstick: add new defaults for `<p>` + more
    Adds new default styling for any `<p>`, `<a>` and `<li>` tags within a page's content area  (`<Section/>`)

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Section from "./lib/components/primitives/Section";
 import { CONFERENCE_DATES } from "./lib/config/homepage.config";
 import { createMetadata } from "./lib/utils/createMetadata";
 
-export const metadata = createMetadata({title: 'Home'})
+export const metadata = createMetadata({ title: "Home" });
 export default function Home() {
   return (
     <>
@@ -14,26 +14,31 @@ export default function Home() {
         <Hero />
         <Container id="content">
           <Section title="ASSETS at a Glance" spacing={"top-only"}>
-            <div className="flex flex-col gap-y-4">
-              <p className="h3">{`The 26th International ACM SIGACCESS Conference on Computers and Accessibility.`}</p>
-              <p>{`The ASSETS conference is the premier forum for presenting research on the design, evaluation, use, and education related to computing for people with disabilities and older adults. For those in Europe and Oceania, ASSETS is rated as Core A — a designation for the top academic conferences that are "highly respected in a discipline area" (Core A; Top 16%).`}</p>
-              <p>
-                {`We invite high-quality original submissions on topics relevant to computing and accessibility. All contributions are peer-reviewed by an international Program Committee. Accepted papers and the abstracts for posters and demonstrations, experience reports, and the student research competition will be archived in the ACM Digital Library. Authors of selected papers will be invited to submit extended versions of their papers to a special issue of the`}{" "}
-                <Link
-                  href="https://dl.acm.org/journal/taccess"
-                  target="_blank"
-                  colour={"primary"}
-                >{`ACM Transactions on Accessible Computing (TACCESS)`}</Link>
-                .
-              </p>
-            </div>
+            {/* <div className="flex flex-row gap-x-12"> */}
+              {/* <div className="flex flex-col gap-y-4"> */}
+                <p className="h3">{`The 26th International ACM SIGACCESS Conference on Computers and Accessibility.`}</p>
+                <p>{`The ASSETS conference is the premier forum for presenting research on the design, evaluation, use, and education related to computing for people with disabilities and older adults. For those in Europe and Oceania, ASSETS is rated as Core A — a designation for the top academic conferences that are "highly respected in a discipline area" (Core A; Top 16%).`}</p>
+                <p>
+                  {`We invite high-quality original submissions on topics relevant to computing and accessibility. All contributions are peer-reviewed by an international Program Committee. Accepted papers and the abstracts for posters and demonstrations, experience reports, and the student research competition will be archived in the ACM Digital Library. Authors of selected papers will be invited to submit extended versions of their papers to a special issue of the`}{" "}
+                  <Link
+                    href="https://dl.acm.org/journal/taccess"
+                    target="_blank"
+                    colour={"primary"}
+                  >{`ACM Transactions on Accessible Computing (TACCESS)`}</Link>
+                  .
+                </p>
+              {/* </div> */}
+              {/* <div className="object-cover ">
+                <img className="aspect-[9/16] object-cover" src="/assets/assets23-img-2.jpg" />
+              </div> */}
+            {/* </div> */}
           </Section>
           <Section title="Important Dates" spacing={"standard"}>
             <p>
               <strong>All deadlines</strong> are 11:59 P.M. Anywhere on Earth
               (UTC -12:00).
             </p>
-            <DateList dates={null}/>
+            <DateList dates={null} />
           </Section>
         </Container>
       </main>
