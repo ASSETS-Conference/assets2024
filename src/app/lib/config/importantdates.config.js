ca537b819755e0f24330e0894599bc2102b12823
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-01-31 20:21:25 -0500

    refactor: :art: added date to hero, important dates to home page, and defined all conference dates in config file

---
+++ src/app/lib/config/importantdates.config.js
@@ -0,0 +1,235 @@
+const TECHNICAL_PAPERS_DATES = [
+    {
+        month: "March",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Deadline for Requesting Mentors",
+                date: "Wednesday, March 6th, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+    {
+        month: "May",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Technical Paper Submission Deadline",
+                date: "Wednesday, April 24th, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+    {
+        month: "June",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Author Rebuttal Period Opens",
+                date: "Wednesday, June 6th, 2024",
+                time: "11:59PM",
+            },
+            {
+                title: "Author Rebuttal Period Closes",
+                date: "Wednesday, June 12th, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+];
+
+const POSTER_DEMOS_DATES = [
+    {
+        month: "July",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Posters & Demos Submission Deadline",
+                date: "Friday, July 7th, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+    {
+        month: "August",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Notification of Acceptance",
+                date: "Friday, August 4th, 2024",
+                time: "11:59PM",
+            },
+            {
+                title: "Camera Ready Deadline",
+                date: "Friday, August 18th, 2024",
+                time: "11:59PM",
+            },
+            {
+                title: "Video Presentations Submission Deadline",
+                date: "Wednesday, August 30th, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+];
+
+const EXPERIENCE_REPORTS_DATES = [
+    {
+        month: "July",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Experience Reports Submission Deadline",
+                date: "Friday, July 7th, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+    {
+        month: "August",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Notification of Acceptance",
+                date: "Friday, August 4th, 2024",
+                time: "11:59PM",
+            },
+            {
+                title: "Final versions of Experience Reports Deadline",
+                date: "Friday, August 18th, 2024",
+                time: "11:59PM",
+            },
+            {
+                title: "Video Presentations Submission Deadline",
+                date: "Wednesday, August 30th, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+];
+
+const DOCTORAL_CONSORTIUM_DATES = [
+    {
+        month: "July",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Notification of Acceptance",
+                date: "Friday, July 28th, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+    {
+        month: "August",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Camera-ready Deadline",
+                date: "Friday, August 11th, 2024",
+                time: "11:59PM",
+            },
+            {
+                title: "Video Presentation Deadline",
+                date: "Monday, August 28th, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+];
+
+const STUDENT_RESEARCH_DATES = [
+    {
+        month: "July",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Initial Submissions Deadline",
+                date: "Monday, July 10th, 2024",
+                time: "11:59PM",
+            },
+            {
+                title: "Acceptance Decisions Released",
+                date: "Monday, July 31st, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+    {
+        month: "August",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Final Versions Deadline",
+                date: "Monday, August 14, 2024",
+                time: "11:59PM",
+            },
+            {
+                title: "Video Presentations Submission Deadline",
+                date: "Wednesday, August 30, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+];
+
+const WORKSHOP_PROPOSAL_DATES = [
+    {
+        month: "July",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Submission deadline",
+                date: "Thursday, July 6, 2024",
+                time: "11:59PM",
+            },
+            {
+                title: "Open Discussion (via PCS)",
+                date: "Sunday, July 7 - Sunday, July 14, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+    {
+        month: "August",
+        year: "2024",
+        eventItems: [
+            {
+                title: "Notification of Acceptance",
+                date: "Thursday, August 3, 2024",
+                time: "11:59PM",
+            },
+            {
+                title: "Workshop Website Online",
+                date: "Friday, August 4, 2024",
+                time: "11:59PM",
+            },
+            {
+                title: "Camera-ready Deadline",
+                date: "Friday, August 18, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+    {
+        month: "October",
+        year: "2024",
+        eventItems: [
+            {
+                title: "ASSETS'24 Workshops",
+                date: "Week of October 22, 2024",
+                time: "11:59PM",
+            },
+        ],
+    },
+];
+
+export {
+    TECHNICAL_PAPERS_DATES,
+    POSTER_DEMOS_DATES,
+    EXPERIENCE_REPORTS_DATES,
+    DOCTORAL_CONSORTIUM_DATES,
+    STUDENT_RESEARCH_DATES,
+    WORKSHOP_PROPOSAL_DATES,
+};
