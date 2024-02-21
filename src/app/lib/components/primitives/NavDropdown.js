b0847ec8ece4b5c3f5a39d9256c5ff37cccedbf0
surajgoraya <SurajGoraya@outlook.com>
2024-02-21 12:28:29 -0500

    fix: links on staging

--- src/app/lib/components/primitives/NavDropdown.js
+++ src/app/lib/components/primitives/NavDropdown.js
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { usePathname } from "next/navigation";
+import Link from "./Link";
 
 const NavDropdown = ({ items, visible }) => {
     const pathname = usePathname();
@@ -10,7 +11,7 @@ const NavDropdown = ({ items, visible }) => {
             <ul className="bg-theme-dark py-4 px-8 md:absolute md:top-16 md:left-0 md:min-w-[220px] flex flex-col gap-2">
                 {items.map((item, i) => (
                     <li key={`nav-dropdown-item-${i}`}>
-                        <a
+                        <Link
                             className={`underline hoctive:decoration-4 ${
                                 pathname === item.href ? "decoration-4" : ""
                             }`}
@@ -18,7 +19,7 @@ const NavDropdown = ({ items, visible }) => {
                             tabIndex="0"
                         >
                             {item.title}
-                        </a>
+                        </Link>
                     </li>
                 ))}
             </ul>
