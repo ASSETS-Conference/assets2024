6346f70d772158477a9bad63ef38149ac44711ba
surajgoraya <SurajGoraya@outlook.com>
2024-03-05 14:41:06 -0500

    Update Image.js

--- src/app/lib/components/primitives/Image.js
+++ src/app/lib/components/primitives/Image.js
@@ -4,6 +4,7 @@ import NextImage from 'next/image'
 export default function Image(props) {
     function adjustSrc(src) {
         const basePath = process.env.NEXT_PUBLIC_IN_GITHUB_ACTIONS ? '/assets2024' : ''
+        console.log(process.env.NEXT_PUBLIC_IN_GITHUB_ACTIONS)
         const url =  src.startsWith('/') ? `${basePath}${src}` :`${basePath}/${src}`  ;
         return `${url}`
     }
