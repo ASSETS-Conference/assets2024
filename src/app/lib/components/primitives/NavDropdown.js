d391e856c5d1af785d34dfdcd9545d8e6423d842
surajgoraya <SurajGoraya@outlook.com>
2024-03-19 15:33:02 -0400

    fix: Mobile Menu closes before Nav on Safari

--- src/app/lib/components/primitives/NavDropdown.js
+++ src/app/lib/components/primitives/NavDropdown.js
@@ -3,16 +3,16 @@ import React from "react";
 import { usePathname } from "next/navigation";
 import Link from "./Link";
 
-const NavDropdown = ({ items, visible }) => {
+const NavDropdown = ({ items, visible, mobileViewVisible }) => {
     const pathname = usePathname();
 
     return (
         visible && (
-            <ul className="bg-theme-dark py-4 px-8 md:absolute md:top-16 md:left-0 md:min-w-[220px] flex flex-col gap-2">
+            <ul className={`${ mobileViewVisible ? 'min-w-full w-full' : ''} bg-theme-dark py-4 px-8 md:absolute md:top-16 md:left-0 md:min-w-[220px] flex flex-col gap-2`}>
                 {items.map((item, i) => (
-                    <li key={`nav-dropdown-item-${i}`}>
+                    <li className={`${ mobileViewVisible ? 'min-w-full w-full' : ''}`} key={`nav-dropdown-item-${i}`}>
                         <Link
-                            className={`underline hoctive:decoration-4 ${
+                            className={`${ mobileViewVisible ? 'min-w-full w-full inline-block py-2 last:pb-0' : ''} underline hoctive:decoration-4 ${
                                 pathname === item.href ? "decoration-4" : ""
                             }`}
                             href={item.href}
