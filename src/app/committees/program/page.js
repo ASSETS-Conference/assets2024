654c32fc459dbe93ed2c53b3bc0ba19b41eaa2d0
surajgoraya <SurajGoraya@outlook.com>
2024-04-03 12:55:37 -0400

    fix: :hamster: small fix with sorting of names

--- src/app/committees/program/page.js
+++ src/app/committees/program/page.js
@@ -1,6 +1,7 @@
 import Section from "@/app/lib/components/primitives/Section";
 import Subpage from "@/app/lib/components/templates/Subpage";
 import { PROGRAM_COMMITTEE } from "@/app/lib/config/committees.config";
+import { SortNameAlphabetically } from "@/app/lib/utils/basics";
 import React from "react";
 
 export default function ProgramCommittee() {
@@ -14,7 +15,7 @@ export default function ProgramCommittee() {
                 title={"Program Committee Members"}
                 spacing={"bottom-only"}
             >
-                {PROGRAM_COMMITTEE.map((e, i) => (
+                {PROGRAM_COMMITTEE.sort((a,b)=>SortNameAlphabetically(a,b)).map((e, i) => (
                     <p className="mb-4 text-lg" key={i}>
                         <strong>{e.name + ", "}</strong>
                         <em>
