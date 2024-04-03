0b81b9199de5cec097770ea0181648bb929641ab
surajgoraya <SurajGoraya@outlook.com>
2024-04-03 14:38:05 -0400

    feat: add new chairs

--- src/app/lib/components/Persona.js
+++ src/app/lib/components/Persona.js
@@ -15,16 +15,17 @@ export default function Persona({
       id="persona"
       className="bg-theme-dark text-theme-off-white w-64  flex flex-col"
     >
-      <div className="min-w-fill">
+      <div className={`min-w-fill ${!picture ? 'h-0' : ""}`}>
+        {picture ? 
         <Image
           src={`${picture}`}
           alt={`Headshot image of ${name.split(" ")[0]}`}
-          className=" min-w-full min-h-64 h-64 max-h-64 object-cover object-top"
+          className="min-w-full min-h-64 h-64 max-h-64 object-cover object-top"
           width={200}
           height={200}
-        />
+        /> : null}
       </div>
-      <div className="p-4 flex flex-col mt-auto mb-auto">
+      <div className={`p-4 flex flex-col ${picture ? 'mt-auto mb-auto' : ''}`}>
         {/* <p className="font-light text-xs">{role}</p> */}
         <p className="font-bold text-2xl mb-1">{name}</p>
         <p className="text-xs italic mb-1">{`${affiliation}, ${location}`}</p>
