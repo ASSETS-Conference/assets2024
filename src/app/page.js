e5407821dda13d98b761cc3a75a3902febc3aa5b
surajgoraya <SurajGoraya@outlook.com>
2024-08-27 17:41:43 -0400

    fix: :hamster: adj. reg alerts to emphasize link

--- src/app/page.js
+++ src/app/page.js
@@ -40,12 +40,12 @@ export default function Home() {
               heading={"Registration is Open"}
             >
               <p className="text-md text-white">
-                {`Full details of our in-person and virtual conference experiences will be published as they become available. In the meantime, we have opened registration on the `}
+                {`We have opened registration on the `}
                 <Link className={"text-white"} href={"https://cvent.me/45nlmw"}>
                   CVENT Registration Site
                 </Link>
 
-                {` to support attendees who may need additional time to make plans (e.g., travel, visa).`}
+                {` to support attendees who may need additional time to make plans (e.g., travel, visa). Full details of our in-person and virtual conference experiences will be published as they continue to become available.`}
               </p>
             </Alert>
             <Alert
