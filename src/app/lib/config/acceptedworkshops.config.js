037c713c212f56529102234a71cc6146af26e904
surajgoraya <SurajGoraya@outlook.com>
2024-09-08 16:42:08 -0400

    feat: :sparkles: add accepted workshops

---
+++ src/app/lib/config/acceptedworkshops.config.js
@@ -0,0 +1,77 @@
+export const ACCEPTED_WORKSHOPS = [
+  {
+    title: "The Future of Urban Accessibility: The Role of AI",
+    organizers: [
+      "Jon E. Froehlich",
+      "Chu Li",
+      "Maryam Hosseini",
+      "Fabio Miranda",
+      "Andres Sevtsuk",
+      "Yochai Eisenberg",
+    ],
+    website: "https://accessiblecities.github.io/UrbanAccess2024/",
+    deadline: "Sept 20, 2024",
+    placeDetails: "Week of Oct 21 via zoom (exact date TBD)",
+    placeLink: null,
+    abstract:
+      "We have entered a new era of computing—one where AI permeates every aspect of society from education to healthcare. In this workshop, we examine the emerging role of AI in the design of equitable and accessible cities, transportation systems, and interactive tools for mapping and navigation. We will solicit short papers around key Urban AI + disability themes, including autonomous vehicles, intelligent wheelchairs, assistive human-robotic interaction, assessing and navigating pedestrian pathways, indoor accessibility, and overarching challenges related to ethics, bias, and data privacy and security. We invite both traditional HCI and accessibility researchers as well as scholars and practitioners from other disciplines relevant to this workshop, including disability studies, gerontology, social work, community psychology, and law. Our overarching goal is to identify open challenges, share current work across disciplines, and spur new collaborations related to AI and urban accessibility.",
+  },
+  {
+    title:
+      "Teaching Accessibility in Different Disciplines: Topics, Approaches, Resources, Challenges",
+    organizers: [
+      "Kyrie Zhixuan Zhou",
+      "Rachel F. Adler",
+      "Caterina Almendral",
+      "Soyoung Choi",
+      "Devorah Kletenik",
+      "Bruno Oro",
+      "JooYoung Seo",
+    ],
+    website: " https://sites.google.com/view/teach-a11y/home",
+    deadline: "Monday, September 30, 2024 (11:59 PM AOE)",
+    placeDetails: "Wednesday, October 23, 2024, from 9:00 am to 2:00 pm (CST)",
+    placeLink: null,
+    abstract:
+      "Teaching accessibility is crucial to ensuring that accessibility principles are integrated into the design and adoption of software. The existing literature on accessibility education is largely siloed, appearing primarily in computer science and related disciplines. Understanding the similarities and differences in teaching accessibility across different disciplines is vital for enhancing educational effectiveness by leveraging lessons learned from each field. This workshop aims to serve as an interdisciplinary forum for researchers and education practitioners to discuss topics taught, approaches taken, resources utilized, and challenges encountered when teaching accessibility in different disciplines",
+  },
+  {
+    title:
+      "Challenges and Considerations for Accessibility Research Across Cultures and Regions",
+    organizers: [
+      "Laleh Nourian",
+      "Yulia Goldenberg",
+      "Muhammad Adamu",
+      "Vikram Kamath Cannanure",
+      "Catherine Holloway",
+      "Neha Kumar",
+      "Katharina Reinecke",
+      "Garreth W. Tigwell",
+    ],
+    website: " https://lalehnourian.wixsite.com/access-culture",
+    deadline: "Saturday, Sep 21, 2024",
+    placeDetails: "Oct 24, 1 pm-6 pm Eastern Daylight Time",
+    placeLink: null,
+    abstract:
+      "Postcolonial and decolonial computing examines how technology design and adoption can perpetuate subtle dimensions of coloniality, under-represent certain regions (e.g., the Global South, non-Western regions, Indigenous societies), and marginalize them. Despite the rapid expansion of accessibility research in the last decades, there is yet little focus on accessibility issues within marginalized societies, hindering them from effectively benefiting from accessibility research discussions and outcomes. There has been a recent growing interest in interdisciplinary research focusing on marginalized communities, including accessibility and participatory research with a focus on cultures and countries that are historically under-represented in computing research, such as non-Western cultures, the Global South, and Indigenous people. However, the accessibility and HCI communities still lack comprehensive knowledge on conducting interdisciplinary research that includes diverse cultures and experiences from some of the systematically marginalized regions. This workshop will explore the intersection of accessibility, HCI, and cross-regional studies, bringing together researchers and practitioners to foster collaborations, identify under-explored research areas, and develop guidelines to support inclusive research practices.",
+  },
+  {
+    title: "accessFinTech: Designing Accessible Financial Technology",
+    organizers: [
+      "Jiamin Dai",
+      "Benjamin M. Gorman",
+      "Garreth W. Tigwell",
+      "Helena Lyhme",
+      "Belén Barros Pena",
+      "Karyn Moffatt",
+      "Celine Latulipe",
+    ],
+    website: "https://accessfintechworkshop.github.io",
+    deadline: "Friday, Sep 20, 2024 (11:59 PM AOE)",
+    placeDetails:
+      "Workshop Day 1: Monday, Oct 21, 60min+90min sync sessions; Workshop Day 2: Wednesday, Oct 23, 90min sync session; Workshop Day 3: Friday, Oct 25, 60min sync session",
+    placeLink: null,
+    abstract:
+      "Financial technology (fintech) has a growing impact on economic and social participation due to the increasing adoption of online banking and digital payments in everyday life. As fintech interests emerge in academic and industry work across the globe, critical needs and opportunities arise for ASSETS communities to lead and shape the discourse on accessible fintech. The Designing Accessible Financial Technology (accessFinTech)  will bring together a diverse group of researchers and practitioners interested in developing a research agenda on designing accessible and inclusive fintech. We will take a timely step towards building a community to support continued discussion on the complex cultural and social contexts around fintech. ",
+  },
+];
