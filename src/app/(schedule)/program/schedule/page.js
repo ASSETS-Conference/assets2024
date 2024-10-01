76da557ffd897c5893936eaabd82776980053642
surajgoraya <SurajGoraya@outlook.com>
2024-10-01 15:24:23 -0400

    feat: hint to let the user know to scroll

--- src/app/(schedule)/program/schedule/page.js
+++ src/app/(schedule)/program/schedule/page.js
@@ -2,11 +2,11 @@ import React from "react";
 import Section from "@/app/lib/components/primitives/Section";
 import Subpage from "@/app/lib/components/templates/Subpage";
 import { createMetadata } from "@/app/lib/utils/createMetadata";
-import { ScheduleSection } from "@/app/lib/components/schedule/ScheduleSection";
 import { Schedule } from "@/app/lib/components/schedule/Schedule";
 import { SCHEDULE } from "@/app/lib/config/schedule.config";
-import Footer from "@/app/lib/components/primitives/Footer";
 import { ScheduleGlance } from "@/app/lib/components/schedule/ScheduleGlance";
+import { GoArrowLeft } from "react-icons/go";
+import { GoArrowRight } from "react-icons/go";
 
 export const metadata = createMetadata({ title: "Program & Schedule" });
 
@@ -20,8 +20,16 @@ export default function SchedulePage() {
         noTOC={true}
       >
         <Section spacing={"bottom-only"} title="ASSETS 2024 at a Glance">
-          <div className="flex justify-center">
+          <div className="flex flex-col items-center overflow-y-auto">
             <ScheduleGlance />
+          </div>
+          <div
+            id="scroll-hint"
+            className="sm:hidden flex items-center justify-center align-middle gap-2 text-black/70"
+          >
+            <GoArrowLeft className="mt-4 text-lg" aria-hidden />
+            <p className="text-base font-light">Scroll to view more</p>
+            <GoArrowRight className="mt-4 text-lg" aria-hidden />
           </div>
         </Section>
         <Section spacing="bottom-only" title="Detailed Program" className={""}>
