61c1927447660a5dbcd629fe6751a209460a98e0
Jaipreet Singh <jaipreet.singh1@ontariotechu.net>
2024-10-28 18:43:30 -0400

    feat: :construction: working on hyperlinking

--- src/app/lib/components/schedule/ScheduleSection.js
+++ src/app/lib/components/schedule/ScheduleSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { ScheduleEvent } from "./ScheduleEvent";
 
-export const ScheduleSection = ({ day, background }) => {
+export const ScheduleSection = ({ day, dayIdx, background }) => {
   return (
     <div className="min-w-full">
       <h2 className={`m-0 p-0 md:text-3xl text-lg sticky top-0 md:pb-8 pb-4 pt-8 z-30 ${background}`}>
@@ -13,6 +13,7 @@ export const ScheduleSection = ({ day, background }) => {
             <ScheduleEvent
               background={`${background}`}
               key={"se-" + i}
+              dayIdx={dayIdx}
               title={event.title}
               note={event.note ?? null}
               link={event.link ?? null}
